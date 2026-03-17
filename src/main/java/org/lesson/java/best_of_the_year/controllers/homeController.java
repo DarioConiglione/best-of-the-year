552cc33894555cf6c2ace08c23947647ac1dfa0d
DarioConiglione <dariocng13@gmail.com>
2026-03-17 10:44:55 +0100

    New task with thymeleaf

---
+++ src/main/java/org/lesson/java/best_of_the_year/controllers/homeController.java
@@ -0,0 +1,20 @@
+package org.lesson.java.best_of_the_year.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class homeController {
+
+    @GetMapping("/")
+    public String Home(Model model, @RequestParam(name = "name") String name) {
+
+        model.addAttribute("name", name);
+        return "home";
+    }
+
+}
